ef006661ce45944d081827e73ad87eff788bf372
NataliR55 <rsn7428@yandex.ru>
2023-04-18 22:08:04 +0600

    technical task 11  v1.0

--- src/main/java/ru/yandex/practicum/filmorate/controllers/FilmsController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmsController.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.controllers;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
